0b219ec24307ca3d8978ed4c9a3c07b0b08fdb71
marypaduano <maria.paduano@gmail.com>
2023-02-10 10:18:54 +0100

    fix function reset

--- js/script.js
+++ js/script.js
@@ -1,38 +1,42 @@
 console.log('campo minato')
-
-let sideGrid = 10
-let numCell = sideGrid ** 2
-
-console.log(sideGrid, numCell)
-
 const gridElement = document.querySelector('.grid')
-console.log(gridElement)
 const btnElement = document.querySelector('.btn-play')
 
-btnElement.addEventListener('click', function (){
+btnElement.addEventListener('click', play)
 
-for (let i = 0; i < numCell; i++) {
-    let n = i + 1
-    console.log(n)
-    let divString = `<div class="cell" style="width: calc(100% / ${sideGrid});" >${n}</div>`
-    gridElement.innerHTML += divString
+function onClick(el) {
+    el.classList.toggle('cell-click')
 }
 
+function play (){
+console.log('play')
+reset ()
+gridGenerate()
 const cellElements = document.querySelectorAll('.cell')
-
 for (let i = 0; i < cellElements.length; i++) {
     const cell = cellElements[i]
 
     cell.addEventListener('click', function () {
-        console.log(i + 1)
         onClick(cell)
     })
 }
-
-})
-
-function onClick(el) {
-    el.classList.toggle('cell-click')
-    return
 }
+
+//FUNCTION REC
+function reset (){
+    gridElement.innerHTML = ''
+}
+
+function gridGenerate (side){
+    let sideGrid = 10
+    let numCell = sideGrid ** 2
+
+for (let i = 0; i < numCell; i++) {
+    let n = i + 1
+    let divString = `<div class="cell" style="width: calc(100% / ${sideGrid});" >${n}</div>`
+    gridElement.innerHTML += divString
+}
+}
+
+
 
